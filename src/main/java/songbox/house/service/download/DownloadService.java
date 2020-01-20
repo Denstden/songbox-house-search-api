ae80b7f290d01c52ecbdf55f332b50db66afe9af
denstden <denstden@gmail.com>
2020-01-20 18:43:33 +0200

    Dev: Remove ClientConfiguration

--- src/main/java/songbox/house/service/download/DownloadService.java
+++ src/main/java/songbox/house/service/download/DownloadService.java
@@ -1,11 +1,10 @@
 package songbox.house.service.download;
 
-import songbox.house.domain.dto.ClientConfiguration;
 import songbox.house.domain.dto.request.SearchQueryDto;
 import songbox.house.domain.dto.response.TrackDto;
 
 import java.util.Optional;
 
 public interface DownloadService {
-    Optional<TrackDto> download(SearchQueryDto searchQuery, ClientConfiguration clientConfiguration);
+    Optional<TrackDto> download(SearchQueryDto searchQuery);
 }
--- src/main/java/songbox/house/service/search/SearchService.java
+++ src/main/java/songbox/house/service/search/SearchService.java
@@ -1,9 +1,8 @@
 package songbox.house.service.search;
 
-import songbox.house.domain.dto.ClientConfiguration;
 import songbox.house.domain.dto.request.SearchQueryDto;
 import songbox.house.domain.dto.response.SearchResultDto;
 
 public interface SearchService {
-    SearchResultDto search(SearchQueryDto searchQuery, ClientConfiguration clientConfiguration);
+    SearchResultDto search(SearchQueryDto searchQuery);
 }
