6baaf8b6d72887039d1285a06a81c9ff0853a5d5
denstden <denstden@gmail.com>
2020-04-03 21:06:30 +0300

    Dev: Add artwork

--- src/main/java/songbox/house/service/download/DownloadService.java
+++ src/main/java/songbox/house/service/download/DownloadService.java
@@ -9,6 +9,8 @@ import java.util.Optional;
 public interface DownloadService {
     Optional<TrackDto> download(SearchQueryDto searchQuery);
 
+    Optional<TrackDto> download(SearchQueryDto searchQuery, String artworkUrl);
+
     Optional<TrackDto> download(TrackMetadataDto trackMetadataDto);
 
     boolean isDownloadEnabled();
